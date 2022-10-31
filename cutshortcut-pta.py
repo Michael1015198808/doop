c752c2f50fd88d9eeb9eec638e988f15b9a815da
ysy <ysy@pascal.com>
2022-11-01 07:13:38 +0800

    adding benchmarks

--- cutshortcut-pta.py
+++ cutshortcut-pta.py
@@ -2,11 +2,8 @@
 import os
 import sys
 
-ALLAPP = ['luindex', 'lusearch', 'fop', 'antlr', 'pmd', 'chart', 'bloat', 'xalan', 'hsqldb', 'eclipse', 'jython',
+ALLAPP = ['bloat', 'xalan', 'hsqldb', 'eclipse', 'jython',
          'findbugs', 'soot', 'gruntspud', 'columba', 'jedit', 'freecol', 'briss']
-DACAPOAPP = ['luindex', 'lusearch', 'fop', 'antlr', 'pmd', 'chart', 'bloat', 'xalan', 'hsqldb', 'eclipse', 'jython']
-EXPAPP = ['bloat', 'xalan', 'hsqldb', 'eclipse', 'findbugs', 'soot', 'gruntspud', 'columba', 'jedit', 'freecol', 'jython', 'briss']
-TMPAPP = ['jython', 'briss', 'jedit', 'freecol']
 ANALYSIS = ['context-insensitive', '2-object-sensitive+heap', '2-type-sensitive+heap', 'zipper-e', 'cut-shortcut', 'collection-3obj']
 
 APPINPUT = {
@@ -16,7 +13,13 @@ APPINPUT = {
     'columba' : 'benchmarks/columba/1.4/columba.jar',
     'jedit' : 'benchmarks/jedit/3.0/jedit.jar',
     'freecol' : 'benchmarks/freecol/0.10.3/FreeCol.jar',
-    'briss' : 'benchmarks/briss/0.9/briss-0.9.jar'
+    'briss' : 'benchmarks/briss/0.9/briss-0.9.jar',
+    'hsqldb' : 'benchmarks/dacapo-2006/hsqldb.jar',
+    'eclipse' : 'benchmarks/dacapo-2006/eclipse.jar',
+    'xalan' : 'benchmarks/dacapo-2006/xalan.jar',
+    'bloat' : 'benchmarks/dacapo-2006/bloat.jar',
+    'jython' : 'benchmarks/dacapo-2006/jython.jar'
+
 }
 
 APPLIB = {
@@ -25,7 +28,12 @@ APPLIB = {
     'columba' : 'benchmarks/columba/1.4/lib benchmarks/columba/1.4/native/linux',
     'jedit' : 'benchmarks/jedit/3.0/lib',
     'freecol' : 'benchmarks/freecol/0.10.3/jars',
-    'briss' : 'benchmarks/briss/0.9'
+    'briss' : 'benchmarks/briss/0.9',
+    'hsqldb' : 'benchmarks/dacapo-2006/hsqldb-deps.jar',
+    'eclipse' : 'benchmarks/dacapo-2006/eclipse-deps.jar',
+    'xalan' : 'benchmarks/dacapo-2006/xalan-deps.jar',
+    'bloat' : 'benchmarks/dacapo-2006/bloat-deps.jar',
+    'jython' : 'benchmarks/dacapo-2006/jython-deps.jar'
 }
 
 APPTAMIFLEX = {
@@ -35,7 +43,12 @@ APPTAMIFLEX = {
     'columba' : 'benchmarks/columba/1.4/refl.log',
     'jedit' : 'benchmarks/jedit/3.0/refl.log',
     'freecol' : 'benchmarks/freecol/0.10.3/refl.log',
-    'briss' : 'benchmarks/briss/0.9/refl.log'
+    'briss' : 'benchmarks/briss/0.9/refl.log',
+    'hsqldb' : 'benchmarks/dacapo-2006/hsqldb-refl.log',
+    'eclipse' : 'benchmarks/dacapo-2006/eclipse-refl.log',
+    'xalan' : 'benchmarks/dacapo-2006/xalan-refl.log',
+    'bloat' : 'benchmarks/dacapo-2006/bloat-refl.log',
+    'jython' : 'benchmarks/dacapo-2006/jython-refl.log'
 }
 
 APPMAIN = {
@@ -45,13 +58,18 @@ APPMAIN = {
     'columba' : 'org.columba.core.main.Main',
     'jedit' : 'org.gjt.sp.jedit.jEdit',
     'freecol' : 'net.sf.freecol.FreeCol',
-    'briss' : 'at.laborg.briss.Briss'
+    'briss' : 'at.laborg.briss.Briss',
+    'hsqldb' : 'Harness',
+    'eclipse' : 'Harness',
+    'xalan' : 'Harness',
+    'bloat' : 'Harness',
+    'jython' : 'Harness'
 }
+
+DOOPHOME = os.path.dirname(os.path.realpath(__file__))
 
 
 # This script should be executed from the root directory of Doop.
-
-
 # ---------------------------------------------------------
 
 RESET = '\033[0m'
@@ -59,24 +77,21 @@ YELLOW = '\033[33m'
 BOLD = '\033[1m'
 
 def runDoop(app, analyse):
+    os.environ['DOOP_PLATFORMS_LIB'] = os.path.join(DOOPHOME, 'benchmarks')
     cmd = ''
     if analyse  == 'zipper-e':
         cmd = cmd + 'python bin/zippere.py -a 2-object-sensitive+heap'
     else :    
         cmd = cmd +"./doop -a "+analyse
-    if app in DACAPOAPP:
-        cmd = cmd + ' -i benchmarks/dacapo-2006/'+app+'.jar --dacapo --platform java_6 --cs-library --no-merge-library-objects --Xno-ssa'
-    else:
-        cmd = cmd + ' -i '+APPINPUT[app]
-        if APPLIB.has_key(app):
-            cmd = cmd + ' -l '+APPLIB[app]
-        cmd = cmd + ' --platform java_6'
-        cmd = cmd + ' --tamiflex '+APPTAMIFLEX[app]
-        cmd = cmd + ' --main '+APPMAIN[app]
-        cmd = cmd + ' --cs-library'
-        cmd = cmd + ' --no-merge-library-objects'
-        cmd = cmd + ' --Xno-ssa'
-#        cmd = cmd + ' --souffle-jobs 8'
+    cmd = cmd + ' -i '+APPINPUT[app]
+    if APPLIB.has_key(app):
+        cmd = cmd + ' -l '+APPLIB[app]
+    cmd = cmd + ' --platform java_6'
+    cmd = cmd + ' --tamiflex '+APPTAMIFLEX[app]
+    cmd = cmd + ' --main '+APPMAIN[app]
+    cmd = cmd + ' --cs-library'
+    cmd = cmd + ' --no-merge-library-objects'
+    cmd = cmd + ' --Xno-ssa'
     print YELLOW + BOLD + 'Running ' + analyse+ ' for '+app+"...."+RESET
     print cmd
     os.system(cmd)
