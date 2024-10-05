af429da568a6485c258239938d5666192e3c8d29
Bluesbreaker45 <bluesbreaker45@outlook.com>
2024-10-05 18:41:12 +0800

    adapt run-zipper to ziper-ins.

--- run-zipper.py
+++ run-zipper.py
@@ -25,7 +25,7 @@ ANALYSES = [
 
 APP = 'temp'
 
-ZIPPER_HOME = './zipper'
+ZIPPER_HOME = '../zipper/zipper'
 ZIPPER_CP = ':'.join([
     os.path.join(ZIPPER_HOME, 'build', 'zipper.jar'),
     os.path.join(ZIPPER_HOME, 'lib', 'guava-23.0.jar'),
@@ -36,7 +36,7 @@ ZIPPER_PTA = 'ptatoolkit.zipper.doop.DoopPointsToAnalysis'
 ZIPPER_CACHE = 'cache/zipper'
 ZIPPER_OUT = 'results'
 
-ZIPPER_THREAD = multiprocessing.cpu_count() # use multithreading to accelerate Zipper
+ZIPPER_THREAD = 4 # multiprocessing.cpu_count() # use multithreading to accelerate Zipper
 ZIPPER_MEMORY = '48g'
 
 REQUIRED_QURIES = [
@@ -55,8 +55,10 @@ RESET = '\033[0m'
 YELLOW = '\033[33m'
 BOLD = '\033[1m'
 
+INS_LEVEL = False
+
 def runPreAnalysis(initArgs):
-    args = [DOOP, "--Xzipper-pre"] + [a if a not in ANALYSES else PRE_ANALYSIS for a in initArgs]
+    args = [DOOP, "--Xzipper-pre", "--Xzipper-ins-tracker" if INS_LEVEL else ""] + [a if a not in ANALYSES else PRE_ANALYSIS for a in initArgs]
     cmd = ' '.join(args)
     print(YELLOW + BOLD + 'Running pre-analysis ...' + RESET)
     print("Pre-analysis cmd:")
@@ -68,10 +70,18 @@ def runZipper(app, cache_dir, out_dir, express):
     suffix = ''
     if express:
         suffix = '-express'
-    zipper_file = os.path.join(out_dir, \
-        '%s-ZipperPrecisionCriticalMethod%s.facts' % (app, suffix))
-    if os.path.exists(zipper_file):
-        os.remove(zipper_file) # remove old file
+    zipper_ins_dic = {
+        "methods": os.path.join(out_dir, '%s-ZipperPrecisionCriticalMethod%s.facts' % (app, suffix)),
+        "heap_allocation": os.path.join(out_dir, '%s-InsAssignHeapAllocation.facts' % app),
+        "local_assign": os.path.join(out_dir, '%s-InsAssignLocal.facts' % app),
+        "casts": os.path.join(out_dir, '%s-InsAssignCast.facts' % app),
+        "instance_load": os.path.join(out_dir, '%s-InsLoadInstanceField.facts' % app),
+        "array_load": os.path.join(out_dir, '%s-InsLoadArrayIndex.facts' % app),
+    }
+
+    for file in zipper_ins_dic.values():
+        if os.path.exists(file):
+            os.remove(file) # remove old file
 
     cmd = 'java -Xmx%s ' % ZIPPER_MEMORY
     cmd += ' -cp %s ' % ZIPPER_CP
@@ -80,6 +90,8 @@ def runZipper(app, cache_dir, out_dir, express):
     cmd += ' -app %s ' % app
     cmd += ' -cache %s ' % cache_dir
     cmd += ' -out %s ' % out_dir
+    if INS_LEVEL:
+        cmd += '-ins-level' # not '--ins-level'
     # cmd += ' -debug'
     if ZIPPER_THREAD > 1:
         cmd += ' -thread %d ' % ZIPPER_THREAD
@@ -88,10 +100,20 @@ def runZipper(app, cache_dir, out_dir, express):
     print("Zipper analysis cmd:")
     print(cmd)
     os.system(cmd)
-    return zipper_file
+    return zipper_ins_dic
 
-def runMainAnalysis(args, zipper_file):
-    args = [DOOP, '--Xzipper', zipper_file] + args # you may add '--cache' between DOOP and '--Xzipper'
+def runMainAnalysis(args, zipper_ins_file_dic):
+    zipper_ins_args = [
+        '--instruction-level',
+        '--ins-assignheapallocation', zipper_ins_file_dic['heap_allocation'],
+        '--ins-assignlocal', zipper_ins_file_dic['local_assign'],
+        '--ins-assigncast', zipper_ins_file_dic['casts'],
+        '--ins-loadinstancefield', zipper_ins_file_dic['instance_load'],
+        '--ins-loadarrayindex', zipper_ins_file_dic['array_load'],
+    ]
+    args = [DOOP, '--Xzipper', zipper_ins_file_dic['methods']] \
+            + (zipper_ins_args if INS_LEVEL else []) \
+            + args # you may add '--cache' between DOOP and '--Xzipper'
     cmd = ' '.join(args)
     print(YELLOW + BOLD + 'Running main (Zipper-guided) analysis ...' + RESET)
     print("Main analysis cmd:")
@@ -122,6 +144,16 @@ def run(args):
                     i += 1
                 else:
                     express = 0.05 # default threshold for Zipper-e
+            elif args[i] == '--ins-level':
+                REQUIRED_QURIES.extend(['ASSIGN_CAST', 'INSTANCE_LOAD_CAUSE', 'ARRAY_LOAD_CAUSE'])
+                global INS_LEVEL
+                INS_LEVEL = True
+            elif args[i] == "--souffle-jobs":
+                global ZIPPER_THREAD
+                ZIPPER_THREAD = int(args[i+1])
+                res.append(args[i])
+                res.append(args[i+1])
+                i += 1
             else:
                 res.append(args[i])
             i += 1
@@ -139,8 +171,8 @@ def run(args):
     runPreAnalysis(args)
     movePreAnalysisResult()
     # dumpRequiredDoopResults(APP, DATABASE, ZIPPER_CACHE)
-    zipper_file = runZipper(APP, ZIPPER_CACHE, ZIPPER_OUT, express)
-    runMainAnalysis(args, zipper_file)
+    zipper_in_file_dic = runZipper(APP, ZIPPER_CACHE, ZIPPER_OUT, express)
+    runMainAnalysis(args, zipper_in_file_dic)
 
 if __name__ == '__main__':
     """
